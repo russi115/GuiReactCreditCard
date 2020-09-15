0200c4c096a9da1a10605dab0d1586eb90f75a78
russi115 <shamanse2001@gmail.com>
2020-09-15 07:10:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <div className="bg-secondary-text-white text-center p-2 " >
+        <table className="table table-striped table-bordered">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name on Card"
+          />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Card Number"
+          />
+          <div className="float-right">
+            <input
+              type="text"
+              className="form-control w-26"
+              placeholder="Valid throught"
+            />
+          </div>
+          <div className="float-left">
+            <input
+              type="text"
+              className="form-control w-26"
+              placeholder="CVV"
+              maxLength="16"
+            />
+          </div>
+
+        </table>
+      </div>
+      <button className="btn btn-primary mt-1">
+        Pay
+         </button>
+    </div >
   );
 }
 
